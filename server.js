19ac55643293feb293812d5da44d96e3e21d7cfa
Anmol Khandekar <anmol.khandekar92@gmail.com>
2020-02-14 01:30:20 -0600

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const env = require('dotenv')
-var Twit = require('twit');
+const Twit = require('twit');
+const path = require('path');
 
 env.config();
 
@@ -46,6 +47,15 @@ app.get('/search', (req,res) => {
   });
    
 });
+
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 
